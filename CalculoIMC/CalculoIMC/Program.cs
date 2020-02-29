8f7810bdad300560d78bed889286be8440936575
Daniel <daniel.vieck@outlook.com>
2020-02-29 16:42:27 -0300

    Exercícios da primeira aula

---
+++ CalculoIMC/CalculoIMC/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CalculoIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pegar peso do usuário
+            Console.WriteLine("Insira seu peso");
+            var peso = BuscaValor();
+            if (peso == null) Console.WriteLine("Valor inválido!");
+
+            // pegar altura do usuário
+            Console.WriteLine("Insira sua altura");
+            var altura = BuscaValor();
+            if (altura == null)
+            {
+                Console.WriteLine("Valor inválido!");
+            }
+            var imc = (double)peso.Value / Math.Pow((double)altura.Value, 2);
+
+            Console.WriteLine("Seu IMC atual é: " + Math.Round(imc, 2));
+
+            Console.ReadLine();
+        }
+
+        private static decimal? BuscaValor()
+        {
+            if (!decimal.TryParse(Console.ReadLine(), out decimal valor))
+                return null;
+
+            return valor;
+        }
+    }
+}
---
+++ FuncoesBasicas/FuncoesBasicas/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FuncoesBasicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira seu nome completo");
+            var nome = Console.ReadLine();
+
+            Console.WriteLine("Insira sua idade");
+            var idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insira seu telefone com DDD (somente numeros)");
+            var telefone = Console.ReadLine();
+
+            Console.WriteLine("Telefone formatado: " + FormataTelefone(telefone));
+            Console.WriteLine("Raiz quadrada do nome: " + CalculaRaizQuadrada(idade));
+            Console.WriteLine("Sem espaços: " + TrocaEspacos(nome));
+
+            Console.ReadLine();
+        }
+
+        private static string TrocaEspacos(string nome)
+        {
+            return nome.Replace(" ", ";");
+        }
+
+        private static double CalculaRaizQuadrada(int idade)
+        {
+            return Math.Round(Math.Sqrt(idade), 2);
+        }
+
+        private static string FormataTelefone(string telefone)
+        {
+            var ddd = telefone.Substring(0, 2);
+            var primeiraParte = telefone.Substring(2, 5);
+            var segundaParte = telefone.Substring(7);
+
+            return string.Format("({0}) {1}-{2}", ddd, primeiraParte, segundaParte);
+        }
+    }
+}
